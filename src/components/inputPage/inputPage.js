89ca9c76a5293a52c12474437f19e29ffe7e8122
FrostDev <frostdev123friday@gmail.com>
2023-09-16 12:58:12 -0400

    test updating

--- src/components/inputPage/inputPage.js
+++ src/components/inputPage/inputPage.js
@@ -1,6 +1,8 @@
 import { faAngleDoubleRight } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { useState } from "react";
+import { useEffect, useState, useLayoutEffect } from "react";
+
+import { motion } from "framer-motion";
 
 function Explanation() {
     return (
@@ -14,7 +16,7 @@ function Explanation() {
 }
 
 
-function Prompt({setPage}) {
+function Prompt() {
     const [promptContent, setPromptContent] = useState('');
 
     function SendPrompt()
@@ -29,7 +31,7 @@ function Prompt({setPage}) {
                 onChange={e => setPromptContent(e.target.value)}
             />
             <button className="group bg-100 hover:bg-blue-200 text-500 font-bold rounded w-20 h-9 p-2"
-                    onClick={() => {SendPrompt(); setPage(2)}}>
+                    onClick={SendPrompt}>
                 <div className="flex flex-row items-center h-full w-200 gap-3 group-hover:-translate-x-20 transition-all pl-1">
                     <p className="pr-9">Submit</p>
                     <FontAwesomeIcon icon = {faAngleDoubleRight}></FontAwesomeIcon>
@@ -39,13 +41,104 @@ function Prompt({setPage}) {
     )
 }
 
-export default function InputPage({setPage}) {
+
+const ActionTest = [
+    {
+        time: 0,
+        x: 10,
+        y: 0,
+    },
+    {
+        time: 4,
+        x: 200,
+        y: 100,
+    },
+    {
+        time: 3,
+        x: 400,
+        y: 0,
+    },
+    {
+        time: 9,
+        x: -20,
+        y: 100,
+    },
+]
+
+function IDK({ actions, time })
+{
+    const [x, setX] = useState(0);
+    const [y, setY] = useState(0);
+
+    function lerp(a, b, t)
+    {
+        return a + t * (b - a);
+    }
+
+    function update()
+    {
+        var localT = time;
+        var currentAction = 0;
+
+        for (var cur = actions.length - 1; cur >= 0; --cur) {
+            if (localT >= actions[cur].time) {
+                localT -= actions[cur].time;
+                currentAction = cur;
+                break;
+            }
+        }
+
+        var actionTime = currentAction === actions.length - 1 ? 0 :
+            actions[currentAction + 1].time - actions[currentAction].time;
+
+        var t = localT / actionTime;
+
+        if (actionTime == 0) {
+            // clip
+            // --currentAction;
+            // localT = actions[currentAction].time;
+        }
+        else {
+            console.log(time)
+            setX(lerp(actions[currentAction].x, actions[currentAction + 1].x, t));
+            setY(lerp(actions[currentAction].y, actions[currentAction + 1].y, t));
+        }
+    }
+    useEffect(()=> {
+        update()
+    },[time])
+
+    return (
+        <div className="w-full">
+            <motion.div
+                className="w-20 h-20 bg-red-600"
+                animate={{x, y}}
+            />
+        </div>
+    )
+}
+
+
+export default function InputPage() {
+    const [startTime, setStartTime] = useState(Date.now())
+    const [time, setTime] = useState(0)
+    const refreshRate = 60
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime((Date.now() - startTime) / 1000);
+        }, 1000/refreshRate);
+
+        return () => clearInterval(interval);
+    }, []);
+
     return (
         <>
-            <div className="p-20 pt-10 bg-500 h-[calc(100vh-70px)] flex flex-col items-center text-100 font-sg">
+            <div className="p-20 bg-500 min-h-screen flex flex-col items-center text-100 font-sg">
                 <div className="flex flex-col w-4/5 max-w-4xl gap-7 ">
                     <Explanation />
-                    <Prompt setPage={setPage} />
+                    <Prompt />
+                    <IDK actions={ActionTest} time={time}/>
                 </div>
             </div>
         </>
