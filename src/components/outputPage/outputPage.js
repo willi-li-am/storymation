11e7c1bbedb295e4da8e528015d7e2bd18fc9b23
William Li <52115161+Trollermaner@users.noreply.github.com>
2023-09-17 02:35:37 -0400

    added backend compatibility

--- src/components/outputPage/outputPage.js
+++ src/components/outputPage/outputPage.js
@@ -1,114 +1,13 @@
 import { useState } from "react";
 import AnimateFrame from "./videoFrame";
 
-// Test raw-scene (expected format)
-const rawScene = 
-{
-    Background: "A cozy, well-lit kitchen with pots and pans hanging overhead, a wooden table in the center, and a refrigerator to the right",
-    length: 6,
-    Characters: [
-        {
-            Type: "Human",
-            Description: "A male robber, dressed in all black with a mask and a sack",
-            Size: "25%",
-            Actions: [
-                {
-                    Position: "50%, 50%",
-                    time: "0"
-                },
-                {
-                    Position: "20%, 5%",
-                    time: "2"
-                },
-                {
-                    Position: "85%, 50%",
-                    time: "6"
-                }
-            ]
-        },
-        {
-            Type: "Human",
-            Description: "A mother, dressed in an apron, holding a frying pan",
-            Size: "25%",
-            Actions: [
-                {
-                    Position: "80%, 50%",
-                    time: "0"
-                },
-                {
-                    Position: "40%, 60%",
-                    time: "3"
-                },
-                {
-                    Position: "20%, 10%",
-                    time: "6"
-                }
-            ]
-        }
-    ]
-}
-
-let scenes = []
-let sceneCount = 0
-
-function Receive(json)
-{
-    let obj = JSON.parse(json);
-    let type = obj["type"];
-    let val = obj["value"];
-
-    if (type === "numberScenes")
-    {
-        sceneCount = val;
-    }
-    else if (type === "scene")
-    {
-        // Parse into friendly scene type
-        let newScene = {
-            Background: val["Background"],
-            length: val["length"],
-            Characters: [],
-        }
-
-        for (let character of val["Characters"])
-        {
-            let newCharacter = {
-                Size: parseFloat(character["Size"].split("%")[0]) / 100,
-                Actions: []
-                // Sprite ???
-            }
-
-            for (let action of character["Actions"])
-            {
-                const pos = action["Position"].split(",");
-                let newAcc = {
-                    x: parseFloat(pos[0]) / 100,
-                    y: parseFloat(pos[1]) / 100,
-                    time: parseFloat(action["time"]),
-                }
-                newCharacter["Actions"].push(newAcc);
-            }
-
-            newScene["Characters"].push(newCharacter);
-        }
-
-        scenes.push(newScene);
-    }
-}
-
 export default function OutputPage(props){
-
-    Receive(JSON.stringify({type: "numberScenes", value: 1}));
-    Receive(JSON.stringify({type: "scene", value: rawScene}));
-    //rawScene["Characters"][0]["Actions"]["Position"] = "0%, 0%";
-    //rawScene["Characters"][1]["Actions"]["Position"] = "0%, 0%";
-    Receive(JSON.stringify({type: "scene", value: rawScene}));
 
     const [currentScene, setCurrentScene] = useState(0);
 
     return(
         <div className="w-full h-full min-h-screen flex flex-col justify-around items-center bg-500 overflow-hidden">
-            <AnimateFrame setPage={props.setPage} currentScene={currentScene} setCurrentScene={setCurrentScene} scenes={scenes}/>
+            <AnimateFrame setPage={props.setPage} setScenes={props.setScenes} currentScene={currentScene} setCurrentScene={setCurrentScene} scenes={props.scenes}  music={props.music} setMusic={props.setMusic} numberScene={props.numberScene} setNumberScene={props.setNumberScene}/>
         </div>
     )
 }
--- src/components/pageFrame/websocketFrame.js
+++ src/components/pageFrame/websocketFrame.js
@@ -8,8 +8,44 @@ let first = false;
 
 export default function SocketFrame({children, newQuery, setNewQuery, setPage, page}){
     const [scenes, setScenes] = useState([])
+    const [numberScene, setNumberScene] = useState(0)
     const [music, setMusic] = useState(false)
     const inputRef = useRef(null)
+
+    function Receive(json)
+    {
+        // Parse into friendly scene type
+        let newScene = {
+            Background: json["Background"],
+            length: json["length"],
+            Characters: [],
+        }
+
+        for (let character of json["Characters"])
+        {
+            let newCharacter = {
+                Size: parseFloat(character["Size"].split("%")[0]) / 100,
+                Actions: [],
+                Link : character["Sprite"]
+            }
+
+            for (let action of character["Actions"])
+            {
+                const pos = action["Position"].split(",");
+                let newAcc = {
+                    x: parseFloat(pos[0]) / 100,
+                    y: parseFloat(pos[1]) / 100,
+                    time: parseFloat(action["time"]),
+                }
+                newCharacter["Actions"].push(newAcc);
+            }
+
+            newScene["Characters"].push(newCharacter);
+        }
+
+        return newScene;
+
+    }
 
     useEffect(() => {
         function openSocket(newQuery){
@@ -37,9 +73,13 @@ export default function SocketFrame({children, newQuery, setNewQuery, setPage, p
                         ws.close()
                         setNewQuery("")
                     }
+                    else if(data["type"] == "numberScenes"){
+                        setNumberScene(data["value"])
+                    }
 
                     else if (data["type"] == "story"){
-                        setScenes([...scenes, data])
+                        const transData = Receive(data)
+                        setScenes([...scenes, transData])
                     }
 
                     else if (data["type"] == "youtube"){
@@ -71,7 +111,7 @@ export default function SocketFrame({children, newQuery, setNewQuery, setPage, p
           <LoadingPage></LoadingPage>
         : <></>}
         {page == 2? 
-          <OutputPage scenes={scenes} setScenes={setScenes} music={music} setMusic={setMusic} setPage={setPage}></OutputPage>
+          <OutputPage scenes={scenes} setScenes={setScenes} music={music} setMusic={setMusic} setPage={setPage} numberScene={numberScene} setNumberScene={setNumberScene}></OutputPage>
         : <></>}
         </>
     )
